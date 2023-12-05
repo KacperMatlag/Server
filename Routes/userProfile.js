7c6088ab975e976245fa82264f97ab36a5928c34
KacperMatlag <matlagkacper@gmail.com>
2023-12-05 23:12:03 +0100

    changes

--- Routes/userProfile.js
+++ Routes/userProfile.js
@@ -15,36 +15,4 @@ router.get("/", async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-router.post("/addRandomUser", async (req, res) => {
-    try {
-        const randomUser = {
-            Login: `user${Math.floor(Math.random() * 100)}`,
-            Password: `password${Math.floor(Math.random() * 100)}`,
-            Profile: {
-                Name: 'John',
-                Surname: 'Doe',
-                DateOfBirth: '1990-01-01',
-                Email: 'john.doe@example.com',
-                PhoneNumber: '123456789',
-                ProfilePicture: 'default.jpg',
-                AddressID: 1,
-                ProfessionalSummary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-            }
-        };
-
-        console.log('Attempting to add random user:', randomUser);
-
-        const createdUser = await User.create(randomUser, {
-            include: [Profile]
-        });
-
-        console.log('Added random user:', createdUser);
-
-        res.json(createdUser);
-    } catch (error) {
-        console.error('Error adding random user and profile:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
 module.exports = router;
